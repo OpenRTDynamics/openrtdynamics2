662a70586938c6d7acd241e0fc28f7a596bc4734
Christian Klauer <chr.klauer@gmail.com>
2019-02-24 21:50:00 +0100

    working on desgin - 1

---
+++ BlockPrototypes.py
@@ -0,0 +1,141 @@
+
+
+from libdyn import *
+from irpar import *
+
+#
+# block definitions
+#
+
+
+
+class Padd(BlockPrototype):
+    def __init__(self, sim : Simulation, inputSignals : List[Signal], fak_list : List[float] ):
+
+        #
+        if len(inputSignals) != 2:
+            raise("inp_list must have exactly 2 elements")
+
+
+        self.fak_list = fak_list
+
+        # outputs. This is a definition of the output ports and the data types known so far
+        OutputDef = OutputDefinitions(  [  DataType( None, None ) ]  )  # None means type and size are not known so far
+
+        #
+        self.blk = Block(sim, self, inputSignals, OutputDef)
+        sim.addBlock(self.blk)
+
+        # call super
+        #BlockPrototype.__init__(self)
+
+    def setPropagatedInputTypes(self, InTypes : InputDefinitions ):
+        # check if inputs are double
+        RequestedType = DataType( ORTD_DATATYPE_FLOAT, 1 )
+
+        if not InTypes.getType(0).isEqualTo( RequestedType ):
+            print("wrong datatype in add function at input port 0")
+
+        if not InTypes.getType(1).isEqualTo( RequestedType ):
+            print("wrong datatype in add function at input port 1")
+
+    # def getOutputTypes(self):
+    #     # shall return OutTypes : OutputDefinitions
+
+    #     return OutputDefinitions(  [  DataType( ORTD_DATATYPE_FLOAT, 1 ) ]  )
+
+
+
+    def GetOutputsSingnals(self):
+        # return the output signals
+        sum = self.blk.GetOutputSignal(0)
+
+        return sum
+
+    def encode_irpar(self):
+        ipar = []
+        rpar = self.fak_list
+
+        return ipar, rpar
+
+    def getORTD_btype(self):
+        # The ORTD interpreter finds the computational function using this id
+        return 12
+
+    def getOperator(self):
+        # don't know if this shall be kept
+        return "add"
+
+
+
+
+
+
+def dyn_add(sim : Simulation, inputSignals : List[Signal], fak_list : List[float]):
+
+    return Padd(sim, inputSignals, fak_list).GetOutputsSingnals()
+
+
+
+class Pconst(BlockPrototype):
+    def __init__(self, sim : Simulation, constant : float ):
+
+        self.constant = constant
+
+        # outputs. This is a definition of the output ports and the data types known so far
+
+
+        # NEXT STEP 22.2.2019: check why these output definition are not considerd
+
+        OutputDef = OutputDefinitions(  [  DataType( ORTD_DATATYPE_FLOAT, 1 ) ]  ) 
+        
+        #
+        self.blk = Block(sim, self, None, OutputDef)
+        sim.addBlock(self.blk)
+
+        # call super
+        #BlockPrototype.__init__(self)
+
+    def setPropagatedInputTypes(self, InTypes : InputDefinitions ):
+        # no inputs
+
+        pass 
+
+    # def getOutputTypes(self):
+    #     # shall return OutTypes : OutputDefinitions
+
+    #     return OutputDefinitions(  [  DataType( ORTD_DATATYPE_FLOAT, 1 ) ]  )
+
+
+
+    def GetOutputsSingnals(self):
+        # return the output signals
+        sum = self.blk.GetOutputSignal(0)
+
+        return sum
+
+    def encode_irpar(self):
+        ipar = []
+        rpar = self.constant
+
+        return ipar, rpar
+
+    def getORTD_btype(self):
+        # The ORTD interpreter finds the computational function using this id
+        return 40
+
+    def getOperator(self):
+        # don't know if this shall be kept
+        return "const"
+
+
+
+def dyn_const(sim : Simulation, constant : float ):
+
+    return Pconst(sim, constant).GetOutputsSingnals()
+
+
+
+
+
+
---
+++ TestExecutable.py
@@ -0,0 +1,113 @@
+
+
+from libdyn import *
+from irpar import *
+from BlockPrototypes import *
+
+
+
+
+@contextmanager
+def ld_subsim(sim):
+    print("create triggered subsimulation" )
+
+    nestedsim = Simulation(sim, 'IFsub')
+
+    yield nestedsim
+
+    nestedsim.FinishSimulation()
+
+    inlist = nestedsim.GetInputInterface()
+
+    print("--- The following inputs go to the nested simulation ---")
+
+    for i in inlist:
+
+        print(  i  )
+
+    # create new block with the inputs in inlist
+
+    # -- Just store which type of subsimulation block to create:
+    #
+    # if / for / ...
+    #
+    # and then do later during export of the schematic
+    #
+    # In general, think about something to store an abstract representation of each block
+    # and do all the work of parameter creation on export
+    #
+    # Maybe use classes for each block, instead of functions (def:)
+    # class functions could be __init__ (collect parameters), check IO, export()
+    #
+    #
+    # (This way, different backends can be supported)
+    #
+
+
+
+    print("finish triggered subsimulation")
+
+
+
+# @contextmanager
+# def ld_IF(sim, ConditionSignal):
+#     print("create triggered subsimulation" )
+
+#     nestedsim = Simulation(sim, 'IFsub')
+
+#     yield nestedsim
+
+#     print("finish triggered subsimulation")
+
+
+
+# new simulation
+sim = Simulation(None, 'main')
+
+
+c1 = dyn_const(sim, 1.123)
+
+c2 = dyn_const(sim, 10)
+
+u = dyn_add(sim, [c1, c2], [1,-1])
+
+
+#dyn_printf(sim, u, "sum value is")
+
+
+
+
+
+print(sim)
+
+# with ld_IF(sim, condition) as sim:
+#     #pass
+#     print("define simulation triggered by if")
+
+
+
+
+
+
+
+# finish
+sim.export_ortdrun('RTMain')
+#sim.ShowBlocks()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
