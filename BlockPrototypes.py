a129f3df708aa08d892bf4103a8d7018d00ac666
Christian Klauer <chr.klauer@gmail.com>
2019-07-11 22:29:31 +0200

    refactor block prototypes - IV

--- BlockPrototypes.py
+++ BlockPrototypes.py
@@ -193,89 +193,6 @@ def dyn_add(sim : Simulation, inputSignals : List[Signal], factors : List[float]
 
 
 
-class Poperator1(BlockPrototype):
-    def __init__(self, sim : Simulation, inputSignals : List[Signal], operator ):
-
-        # 
-        self.inputSignals = inputSignals
-
-        self.operator = operator
-
-        blk = Block(sim, self, inputSignals, blockname = 'operator1').configAddOutputSignal()
-
-
-        # call super
-        BlockPrototype.__init__(self, blk)
-
-        # optional to initially fix the output types:
-        # self.outputSignal(0).setDatatype(  ...  )
-
-
-    def configDefineOutputTypes(self, inputTypes):
-        # print("Padd: in callback configDefineOutputTypes")
-
-
-        if self.outputSignal(0).getDatatype() is None:
-            #
-            # no output type defined so far..
-            # look for an already defined input type and inherit that type.
-            #
-
-            self.outputType = computeResultingNumericType(inputTypes)
-
-            print('Padd (' + self.block.toStr() + '): proposed outputtype of ' + self.outputSignal(0).getName() + ' is: ' + self.outputType.toStr() + '')
-
-        else:
-
-            # check of the given output type is a numeric datatype
-            if not isinstance( self.outputSignal(0).getDatatype(), DataTypeNumeric ):
-                raise BaseException("Poperator1: only DataTypeNumeric can be the result/output of an addition")
-        
-        return [self.outputType]
-
-    def returnDependingInputs(self, outputSignal):
-        # return a list of input signals on which the given output signal depends on
-
-        # the output (there is only one) depends on all inputs
-        return self.inputSignals 
-
-    def returnInutsToUpdateStates(self, outputSignal):
-        # return a list of input signals that are required to update the states
-        return []
-
-    @property
-    def outputSignals(self):
-        # return the output signals
-        output = self.outputSignal(0)
-
-        return output
-
-    def codeGen(self, language, flag):
-
-        lines = ''
-
-        if language == 'c++':
-
-            if flag == 'localvar':
-                lines = self.outputType.cppDataType + ' ' + self.outputSignal(0).getName() + ';\n'
-
-            elif flag == 'output':
-
-                strs = []
-                i = 0
-                for s in self.inputSignals:
-                    strs.append(  str(  s.getName() ) )
-                    i = i + 1
-
-                sumline = (' ' + self.operator + ' ').join( strs )
-
-                lines = self.outputSignal(0).getName() + ' = ' + sumline + ';\n'
-
-        return lines
-
-# def dyn_operator1(sim : Simulation, inputSignals : List[Signal], operator ):
-#     return Poperator1(sim, inputSignals, operator).outputSignals
-
 
 
 
@@ -310,21 +227,21 @@ def dyn_operator1(sim : Simulation, inputSignals : List[Signal], operator : str 
 
 
 
-class Pconst(BlockPrototype):
-    def __init__(self, sim : Simulation, constant, datatype ):
 
-        self.constant = constant
+
+class Source_To1(BlockPrototype):
+    def __init__(self, sim : Simulation, datatype ):
+
         self.outputType = datatype
 
         #
-        blk = Block(sim, self, None, blockname = 'const').configAddOutputSignal()
+        blk = Block(sim, self, None, blockname = '').configAddOutputSignal()
 
         # call super
         BlockPrototype.__init__(self, blk)
 
         # output datatype is fixed
         self.outputSignal(0).setDatatype(datatype)
-
 
     def configDefineOutputTypes(self, inputTypes):
 
@@ -348,25 +265,30 @@ class Pconst(BlockPrototype):
 
         return output
 
-    def codeGen(self, language, flag):
 
-        lines = ''
 
-        if language == 'c++':
 
-            if flag == 'localvar':
-                lines = self.outputType.cppDataType + ' ' + self.outputSignal(0).getName() + ';\n'
+class Const(Source_To1):
+    def __init__(self, sim : Simulation, constant, datatype ):
 
-            elif flag == 'output':
-                lines = self.outputSignal(0).getName() + ' = ' + str( self.constant ) + ';\n'
+        self.constant = constant
 
-        return lines
+        # call super
+        Source_To1.__init__(self, sim, datatype)
+
+    def codeGen_localvar(self, language):
+        return self.outputType.cppDataType + ' ' + self.outputSignal(0).getName() + ';\n'
+
+    def codeGen_output(self, language):
+        return self.outputSignal(0).getName() + ' = ' + str( self.constant ) + ';\n'
 
 
 
 def dyn_const(sim : Simulation, constant, datatype ):
+    return Const(sim, constant, datatype).outputSignals
 
-    return Pconst(sim, constant, datatype).outputSignals
+
+
 
 
 
@@ -446,68 +368,5 @@ def dyn_delay(sim : Simulation, inputSignals : Signal ):
 
 
 
-
-
-
-
-# # becomes obsolete
-# class Pgain(BlockPrototype):
-#     def __init__(self, sim : Simulation, u : Signal, gain : float ):
-
-#         self.u = u
-#         self.gain = gain
-#         self.outputType = None
-
-#         # create a new block
-#         blk = Block(sim, self, [ u ], blockname = 'gain').configAddOutputSignal()
-        
-#         # call super-class constructor
-#         BlockPrototype.__init__(self, blk)
-
-
-#     def configDefineOutputTypes(self, inputTypes):
-
-#         # just inherit the input type 
-#         if inputTypes[0] is not None:
-#             self.outputType = inputTypes[0]
-#         else:
-#             self.outputType = None
-
-#         return [ self.outputType ]        
-
-#     def returnDependingInputs(self, outputSignal):
-#         # return a list of input signals on which the given output signal depends on
-
-#         # the output depends on the only one input signals
-#         return [ self.u ]
-
-#     def returnInutsToUpdateStates(self, outputSignal):
-#         # return a list of input signals that are required to update the states
-#         return []  # no inputs
-
-#     @property
-#     def outputSignals(self):
-#         # return the output signals
-
-#         return self.outputSignal(0)
-
-#     def codeGen(self, language, flag):
-
-#         lines = ''
-
-#         if language == 'c++':
-                
-#             if flag == 'localvar':
-#                 lines = self.outputType.cppDataType + ' ' + self.outputSignal(0).getName() + ';\n'
-
-#             elif flag == 'output':
-#                 lines = self.outputSignal(0).getName() + ' = ' + str(self.gain) + ' * ' + self.inputSignal(0).getName() +  ';\n'
-
-#         return lines
-
-
-# def dyn_gain(sim : Simulation, u : Signal, gain : float ):
-
-#     return Pgain(sim, u, gain).outputSignals
 
 
