e7313fa197778ba0bb19e663164d4e03b85d1e2e
Christian Klauer <chr.klauer@gmail.com>
2019-07-12 17:31:47 +0200

    refactor block prototypes - VI

--- BlockPrototypes.py
+++ BlockPrototypes.py
@@ -4,10 +4,44 @@ from Block import *
 from irpar import *
 
 #
-# block definitions
+# block templates for common use-cases
 #
 
+class Source_To1(BlockPrototype):
+    def __init__(self, sim : Simulation, datatype ):
 
+        self.outputType = datatype
+
+        #
+        blk = Block(sim, self, None, blockname = '').configAddOutputSignal()
+
+        # call super
+        BlockPrototype.__init__(self, blk)
+
+        # output datatype is fixed
+        self.outputSignal(0).setDatatype(datatype)
+
+    def configDefineOutputTypes(self, inputTypes):
+
+        # define the output type 
+        return [ self.outputType ]
+
+    def returnDependingInputs(self, outputSignal):
+        # return a list of input signals on which the given output signal depends on
+
+        # the output depends on nothing
+        return []
+
+    def returnInutsToUpdateStates(self, outputSignal):
+        # return a list of input signals that are required to update the states
+        return []
+
+    @property
+    def outputSignals(self):
+        # return the output signals
+        output = self.outputSignal(0)
+
+        return output
 
 
 
@@ -54,49 +88,6 @@ class StaticFn_1To1(BlockPrototype):
     def codeGen_localvar(self, language):
         if language == 'c++':
             return self.outputType.cppDataType + ' ' + self.outputSignal(0).getName() + ';\n'
-
-
-
-
-class Gain(StaticFn_1To1):
-    def __init__(self, sim : Simulation, u : Signal, factor : float ):
-
-        self._factor = factor
-
-        StaticFn_1To1.__init__(self, sim, u)
-
-    def codeGen_output(self, language):
-        if language == 'c++':
-            return self.outputSignal(0).getName() + ' = ' + str(self._factor) + ' * ' + self.inputSignal(0).getName() +  ';\n'
-
-def dyn_gain(sim : Simulation, u : Signal, gain : float ):
-
-    return Gain(sim, u, gain).outputSignals
-
-
-
-
-
-class StaticFnByName_1To1(StaticFn_1To1):
-    def __init__(self, sim : Simulation, u : Signal, functionName : str ):
-
-        self._functionName = functionName
-
-        StaticFn_1To1.__init__(self, sim, u)
-
-    def codeGen_output(self, language):
-        if language == 'c++':
-            return self.outputSignal(0).getName() + ' = ' + str(self._functionName) + '(' + self.inputSignal(0).getName() +  ');\n'
-
-
-def dyn_sin(sim : Simulation, u : Signal ):
-    return StaticFnByName_1To1(sim, u, 'sin').outputSignals
-
-def dyn_cos(sim : Simulation, u : Signal ):
-    return StaticFnByName_1To1(sim, u, 'cos').outputSignals
-
-
-
 
 
 
@@ -154,153 +145,6 @@ class StaticFn_NTo1(BlockPrototype):
 
 
 
-
-
-class Add(StaticFn_NTo1):
-    def __init__(self, sim : Simulation, inputSignals : List[Signal], factors : List[float] ):
-
-        # feasibility checks
-        if len(inputSignals) != len(factors):
-            raise("len(inp_list) must be equal to len(factors)")
-
-        self.factors = factors
-        StaticFn_NTo1.__init__(self, sim, inputSignals)
-
-    def codeGen_output(self, language):
-
-        if language == 'c++':
-            strs = []
-            i = 0
-            for s in self.inputSignals:
-                strs.append(  str(self.factors[i]) + ' * ' + s.getName() )
-                i = i + 1
-
-            sumline = ' + '.join( strs )
-            lines = self.outputSignal(0).getName() + ' = ' + sumline + ';\n'
-
-            return lines
-
-
-def dyn_add(sim : Simulation, inputSignals : List[Signal], factors : List[float]):
-
-    return Add(sim, inputSignals, factors).outputSignals
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Operator1(StaticFn_NTo1):
-    def __init__(self, sim : Simulation, inputSignals : List[Signal], operator : str ):
-
-        self.operator = operator
-        StaticFn_NTo1.__init__(self, sim, inputSignals)
-
-    def codeGen_output(self, language):
-
-        if language == 'c++':
-            strs = []
-            i = 0
-            for s in self.inputSignals:
-                strs.append(  str(  s.getName() ) )
-                i = i + 1
-
-            sumline = (' ' + self.operator + ' ').join( strs )
-            lines = self.outputSignal(0).getName() + ' = ' + sumline + ';\n'
-
-            return lines
-
-
-def dyn_operator1(sim : Simulation, inputSignals : List[Signal], operator : str ):
-    return Operator1(sim, inputSignals, operator).outputSignals
-
-
-
-
-
-
-
-
-class Source_To1(BlockPrototype):
-    def __init__(self, sim : Simulation, datatype ):
-
-        self.outputType = datatype
-
-        #
-        blk = Block(sim, self, None, blockname = '').configAddOutputSignal()
-
-        # call super
-        BlockPrototype.__init__(self, blk)
-
-        # output datatype is fixed
-        self.outputSignal(0).setDatatype(datatype)
-
-    def configDefineOutputTypes(self, inputTypes):
-
-        # define the output type 
-        return [ self.outputType ]
-
-    def returnDependingInputs(self, outputSignal):
-        # return a list of input signals on which the given output signal depends on
-
-        # the output depends on nothing
-        return []
-
-    def returnInutsToUpdateStates(self, outputSignal):
-        # return a list of input signals that are required to update the states
-        return []
-
-    @property
-    def outputSignals(self):
-        # return the output signals
-        output = self.outputSignal(0)
-
-        return output
-
-
-
-
-class Const(Source_To1):
-    def __init__(self, sim : Simulation, constant, datatype ):
-
-        self.constant = constant
-
-        # call super
-        Source_To1.__init__(self, sim, datatype)
-
-    def codeGen_localvar(self, language):
-        if language == 'c++':
-            return self.outputType.cppDataType + ' ' + self.outputSignal(0).getName() + ';\n'
-
-    def codeGen_output(self, language):
-        if language == 'c++':
-            return self.outputSignal(0).getName() + ' = ' + str( self.constant ) + ';\n'
-
-
-
-def dyn_const(sim : Simulation, constant, datatype ):
-    return Const(sim, constant, datatype).outputSignals
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Dynamic_1To1(BlockPrototype):
     def __init__(self, sim : Simulation, u : Signal ):
 
@@ -343,6 +187,150 @@ class Dynamic_1To1(BlockPrototype):
     def codeGen_localvar(self, language):
         if language == 'c++':
             return self.outputType.cppDataType + ' ' + self.outputSignal(0).getName() + ';\n'
+
+
+
+
+
+
+
+
+
+#
+# Sources
+#
+
+class Const(Source_To1):
+    def __init__(self, sim : Simulation, constant, datatype ):
+
+        self.constant = constant
+
+        # call super
+        Source_To1.__init__(self, sim, datatype)
+
+    def codeGen_localvar(self, language):
+        if language == 'c++':
+            return self.outputType.cppDataType + ' ' + self.outputSignal(0).getName() + ';\n'
+
+    def codeGen_output(self, language):
+        if language == 'c++':
+            return self.outputSignal(0).getName() + ' = ' + str( self.constant ) + ';\n'
+
+def dyn_const(sim : Simulation, constant, datatype ):
+    return Const(sim, constant, datatype).outputSignals
+
+
+
+
+
+#
+# Multiply by constant
+#
+
+class Gain(StaticFn_1To1):
+    def __init__(self, sim : Simulation, u : Signal, factor : float ):
+
+        self._factor = factor
+
+        StaticFn_1To1.__init__(self, sim, u)
+
+    def codeGen_output(self, language):
+        if language == 'c++':
+            return self.outputSignal(0).getName() + ' = ' + str(self._factor) + ' * ' + self.inputSignal(0).getName() +  ';\n'
+
+def dyn_gain(sim : Simulation, u : Signal, gain : float ):
+    return Gain(sim, u, gain).outputSignals
+
+
+
+
+#
+# Basic operators
+#
+
+class Add(StaticFn_NTo1):
+    def __init__(self, sim : Simulation, inputSignals : List[Signal], factors : List[float] ):
+
+        # feasibility checks
+        if len(inputSignals) != len(factors):
+            raise("len(inp_list) must be equal to len(factors)")
+
+        self.factors = factors
+        StaticFn_NTo1.__init__(self, sim, inputSignals)
+
+    def codeGen_output(self, language):
+
+        if language == 'c++':
+            strs = []
+            i = 0
+            for s in self.inputSignals:
+                strs.append(  str(self.factors[i]) + ' * ' + s.getName() )
+                i = i + 1
+
+            sumline = ' + '.join( strs )
+            lines = self.outputSignal(0).getName() + ' = ' + sumline + ';\n'
+
+            return lines
+
+def dyn_add(sim : Simulation, inputSignals : List[Signal], factors : List[float]):
+    return Add(sim, inputSignals, factors).outputSignals
+
+
+
+class Operator1(StaticFn_NTo1):
+    def __init__(self, sim : Simulation, inputSignals : List[Signal], operator : str ):
+
+        self.operator = operator
+        StaticFn_NTo1.__init__(self, sim, inputSignals)
+
+    def codeGen_output(self, language):
+
+        if language == 'c++':
+            strs = []
+            i = 0
+            for s in self.inputSignals:
+                strs.append(  str(  s.getName() ) )
+                i = i + 1
+
+            sumline = (' ' + self.operator + ' ').join( strs )
+            lines = self.outputSignal(0).getName() + ' = ' + sumline + ';\n'
+
+            return lines
+
+
+def dyn_operator1(sim : Simulation, inputSignals : List[Signal], operator : str ):
+    return Operator1(sim, inputSignals, operator).outputSignals
+
+
+
+
+#
+# Static functions that map 1 --> 1
+#
+
+class StaticFnByName_1To1(StaticFn_1To1):
+    def __init__(self, sim : Simulation, u : Signal, functionName : str ):
+
+        self._functionName = functionName
+
+        StaticFn_1To1.__init__(self, sim, u)
+
+    def codeGen_output(self, language):
+        if language == 'c++':
+            return self.outputSignal(0).getName() + ' = ' + str(self._functionName) + '(' + self.inputSignal(0).getName() +  ');\n'
+
+
+def dyn_sin(sim : Simulation, u : Signal ):
+    return StaticFnByName_1To1(sim, u, 'sin').outputSignals
+
+def dyn_cos(sim : Simulation, u : Signal ):
+    return StaticFnByName_1To1(sim, u, 'cos').outputSignals
+
+
+
+#
+# Block that have an internal memory
+#
 
 
 class Delay(Dynamic_1To1):
