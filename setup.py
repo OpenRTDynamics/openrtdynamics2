d128c9b37d67bcd81613f6bc820084147e940886
Christian Klauer <chr.klauer@gmail.com>
2020-04-26 14:43:38 +0200

    towards a library

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openrtdynamics-pkg-christianausb",
+    version="0.0.1",
+    author="Christian Klauer",
+    author_email="chr@gmail.com",
+    description="OpenRTDynamics 2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://openrtdynamics.github.io/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
