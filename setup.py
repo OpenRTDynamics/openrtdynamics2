8d412cc231efeab7e0db1daee7253b78f6e68658
Christian Klauer <chr.klauer@gmail.com>
2020-04-26 15:52:14 +0200

    0.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="openrtdynamics-pkg-christianausb",
+    name="openrtdynamics2",
     version="0.0.1",
     author="Christian Klauer",
     author_email="chr@gmail.com",
