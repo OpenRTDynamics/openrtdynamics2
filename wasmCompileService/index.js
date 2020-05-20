6445f4eebaa088e60d83e3c2a265caea40d79cda
Christian Klauer <chr.klauer@gmail.com>
2020-05-20 21:12:35 +0200

    update compile service - I

--- wasmCompileService/index.js
+++ wasmCompileService/index.js
@@ -1,9 +1,9 @@
-var express        = require("express");
-var bodyParser     = require("body-parser");
-var app            = express();
-const tmp          = require('tmp');
-const { exec }     = require("child_process");
-const fs           = require('fs')
+var express                = require("express");
+var bodyParser             = require("body-parser");
+var app                    = express();
+const tmp_file_generator   = require('tmp');
+const { exec }             = require("child_process");
+const fs                   = require('fs')
 
 var cors = require('cors')
 app.use(cors())
@@ -84,59 +84,56 @@ function compile(tmp_dir, source_code) {
 
     })
 
-
     return {p_wasm : p_wasm, p_jsfile : p_jsfile}
-
 }
 
 
 app.use(bodyParser.json(  limit='5000kb' ) );
 
 
-app.post('/upload', function(req,res) {
+app.post('/compile', function(req,res) {
 
     res.header("Access-Control-Allow-Origin", "*");
-
-    console.log( req.body )
 
     var source_code = req.body.source_code;
     var password = req.body.password;
 
-    console.log(source_code)
-
-    tmp_dir = './uploads'
-
-
-    fs.writeFile(tmp_dir + "/main.cpp",  new Buffer(source_code.main_cpp, 'base64'), function(err) {
-      if (err) {
-
-        return
-      }
-
-      ret = compile(tmp_dir, source_code)
-
-
-      // wait for the compiler
-      ret.p_wasm.then( function(result) {
-
-        rawWebAssembly = result
+    // create a tmp file
+    tmp_file_generator.dir(function _tempDirCreated(err, path, cleanupCallback) {
+      if (err) {return};
     
-        ret.p_jsfile.then( function(result) {
-    
-          jscode = result
-    
-          return_vals = { manifest : "blub", rawWebAssembly : rawWebAssembly.toString('base64'), jscode : jscode.toString('base64') }
-          raw_return_data = JSON.stringify(return_vals)
-        
-          res.setHeader('Content-Type', 'application/json')
-          res.end( raw_return_data )
+      console.log('dir: ', path);
+      tmp_dir = path
+
+      // write the file main.cpp
+      fs.writeFile(tmp_dir + "/main.cpp",  Buffer.from(source_code.main_cpp, 'base64'), function(err) {
+        if (err) {
+          return
+        }
+
+        ret = compile(tmp_dir, source_code)
+
+        // wait for the compiler
+        ret.p_wasm.then( function(result) {
+          rawWebAssembly = result
+      
+          ret.p_jsfile.then( function(result) {
+            jscode = result
+      
+            return_vals = { rawWebAssembly : rawWebAssembly.toString('base64'), jscode : jscode.toString('base64') }
+            raw_return_data = JSON.stringify(return_vals)
+          
+            res.setHeader('Content-Type', 'application/json')
+            res.end( raw_return_data )
+
+            // Manual cleanup
+            cleanupCallback();
+          })
         })
       })
 
+    });
 
-    })
-
-  
 });
 
 
