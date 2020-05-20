7562047c0d3f3b1da1278acd068fee0a66190626
Christian Klauer <chr.klauer@gmail.com>
2020-05-20 20:46:07 +0200

    update compile service

--- wasmCompileService/index.js
+++ wasmCompileService/index.js
@@ -1,25 +1,21 @@
-
-
-
-
-var express        =         require("express");
-var bodyParser     =         require("body-parser");
-var app            =         express();
+var express        = require("express");
+var bodyParser     = require("body-parser");
+var app            = express();
+const tmp          = require('tmp');
+const { exec }     = require("child_process");
+const fs           = require('fs')
 
 var cors = require('cors')
 app.use(cors())
-// app.use(bodyParser.urlencoded({ extended: false }));
 
 
+const config = require('./config.json');
+var emcc_binary = config.emcc_binary
 
-var emcc_binary = '/Users/chr/git/emsdk/fastcomp/emscripten/emcc'
 
-const { exec } = require("child_process");
-const fs = require('fs')
 
 
 function compile(tmp_dir, source_code) {
-
 
   command = '' + emcc_binary + '  --bind -s MODULARIZE=1 -s EXPORT_NAME="ORTD_simulator" main.cpp -g4 -s -o main.js'
 
@@ -58,7 +54,6 @@ function compile(tmp_dir, source_code) {
 
               return
             }
-            //console.log(data)
 
             resolve(data)
         
@@ -66,8 +61,6 @@ function compile(tmp_dir, source_code) {
         })
 
     })
-
-
 
 
     var p_jsfile = new Promise(
@@ -83,7 +76,6 @@ function compile(tmp_dir, source_code) {
 
               return
             }
-            //console.log(data)
 
             resolve(data)
         
@@ -98,12 +90,8 @@ function compile(tmp_dir, source_code) {
 }
 
 
-//compile('blub')
-
-
 app.use(bodyParser.json(  limit='5000kb' ) );
 
-// let parserJSON = bodyParser.json(type='*/*', limit='5000kb')
 
 app.post('/upload', function(req,res) {
 
@@ -121,18 +109,14 @@ app.post('/upload', function(req,res) {
 
     fs.writeFile(tmp_dir + "/main.cpp",  new Buffer(source_code.main_cpp, 'base64'), function(err) {
       if (err) {
-        console.log("error in putting main.cpp")
 
         return
       }
 
-      console.log('placed main.cpp')
-
       ret = compile(tmp_dir, source_code)
 
 
-
-      // wait compiler
+      // wait for the compiler
       ret.p_wasm.then( function(result) {
 
         rawWebAssembly = result
@@ -141,194 +125,30 @@ app.post('/upload', function(req,res) {
     
           jscode = result
     
-    
           return_vals = { manifest : "blub", rawWebAssembly : rawWebAssembly.toString('base64'), jscode : jscode.toString('base64') }
           raw_return_data = JSON.stringify(return_vals)
         
           res.setHeader('Content-Type', 'application/json')
           res.end( raw_return_data )
-        
-    
-    
         })
-    
       })
 
 
     })
 
-
-
-
-  // return_vals = { manifest : "blub", rawWebAssembly : "xxxxx", jscode : "x=1" }
-  // raw_return_data = JSON.stringify(return_vals)
-
-  // res.setHeader('Content-Type', 'application/json')
-  // res.end( raw_return_data )
-
   
 });
 
 
-//app.use(express.static('js'));
 app.use('/js', express.static('js'))
 
 
-// app.post('/upload', parserJSON, function(req,res){
+var port = parseInt(config.node_port)
 
-//   res.header("Access-Control-Allow-Origin", "*");
-
-//   console.log( req.body )
-
-// // var source_code = req.body.main_cpp;
-// // var password = req.body.password;
-
-// // console.log("password is " + password);
-
-// // console.log(source_code)
-
-// res.end("yes");
-// });
-
-
-
-
-app.listen(3000,function(){
-  console.log("Started on PORT 3000");
+app.listen(port,function(){
+  console.log("Listening on port " + port );
 })
 
 
 
 
-
-
-// const express = require('express');
-// const fileUpload = require('express-fileupload');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const morgan = require('morgan');
-// const _ = require('lodash');
-
-// const app = express();
-
-// // enable files upload
-// app.use(fileUpload({
-//     createParentPath: true,
-//     limits: { 
-//         fileSize: 2 * 1024 * 1024 * 1024 //2MB max file(s) size
-//     },
-// }));
-
-// //add other middleware
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(morgan('dev'));
-
-
-// app.post('/upload',function(req,res){
-
-//     var user_name=req.body.user;
-//     var password=req.body.password;
-
-//     console.log(req.body)
-
-
-//     console.log("User name = "+user_name+", password is "+password);
-
-//     res.end("yes");
-// });
-
-
-
-// // upoad single file
-// app.post('/upload-avatar', async (req, res) => {
-//     try {
-//         if(!req.files) {
-//             res.header("Access-Control-Allow-Origin", "*");
-//             //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-//             res.send({
-//                 status: false,
-//                 message: 'No file uploaded'
-                
-//             });
-//         } else {
-//             //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-//             let avatar = req.files.avatar;
-            
-//             //Use the mv() method to place the file in upload directory (i.e. "uploads")
-//             avatar.mv('./uploads/' + avatar.name);
-
-//             //send response
-//             res.header("Access-Control-Allow-Origin", "*");
-//             res.send({
-//                 status: true,
-//                 message: 'File is uploaded',
-//                 data: {
-//                     name: avatar.name,
-//                     mimetype: avatar.mimetype,
-//                     size: avatar.size
-//                 }
-//             });
-//         }
-//     } catch (err) {
-//         res.status(500).send(err);
-//     }
-// });
-
-// // upload multiple files
-// app.post('/upload_sourcecode', async (req, res) => {
-//     try {
-//         console.log(req)
-
-//         if(!req.files) {
-//             res.send({
-//                 status: false,
-//                 message: 'No file uploaded'
-//             });
-
-//             console.log('no file')
-//         } else {
-
-//             console.log('got file')
-
-
-//             let data = []; 
-    
-//             //loop all files
-//             _.forEach(_.keysIn(req.files.photos), (key) => {
-//                 let photo = req.files.photos[key];
-                
-//                 //move photo to upload directory
-//                 photo.mv('./uploads/' + photo.name);
-
-//                 //push file details
-//                 data.push({
-//                     name: photo.name,
-//                     mimetype: photo.mimetype,
-//                     size: photo.size
-//                 });
-//             });
-    
-//             //return response
-//             res.send({
-//                 status: true,
-//                 message: 'Files are uploaded',
-//                 data: data
-//             });
-//         }
-//     } catch (err) {
-//         res.status(500).send(err);
-//     }
-// });
-
-// //make uploads directory static
-// app.use(express.static('uploads'));
-
-// //start app 
-// const port = process.env.PORT || 3000;
-
-// app.listen(port, () => 
-//   console.log(`App is listening on port ${port}.`)
-// );
