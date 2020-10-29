f13f3b47c9cf2036d7909d42294c2f4219547c1d
Christian Klauer <chr.klauer@gmail.com>
2020-10-29 21:43:58 +0100

    cleanup vehicle example

--- example_path_controlled_vehicle2.py
+++ example_path_controlled_vehicle2.py
@@ -1,179 +1,81 @@
 import openrtdynamics2.lang as dy
 
 import os
-import json
-from colorama import init, Fore, Back, Style
-init(autoreset=True)
-
+# 
 import math
 import numpy as np
 
 from vehicle_lib.vehicle_lib import *
-from vehicle_lib.path_generation import *
-
 import vehicle_lib.example_data as example_data
 
 
 #
-# Solving a state space model of a vehicle
+# A vehicle controlled to follow a given path 
 #
-# 
-#
+
 system = dy.enter_system()
 baseDatatype = dy.DataTypeFloat64(1) 
 
-
-
-
-
-# define system inputs
+# define simulation inputs
 velocity       = dy.system_input( baseDatatype ).set_name('velocity').set_properties({ "range" : [0, 50], "unit" : "m/s", "default_value" : 23.75, "title" : "vehicle velocity" })
 k_p            = dy.system_input( baseDatatype ).set_name('k_p').set_properties({ "range" : [0, 4.0], "default_value" : 0.112, "title" : "controller gain" })
 
 disturbance_amplitude  = dy.system_input( baseDatatype ).set_name('disturbance_amplitude').set_properties({ "range" : [-45, 45], "unit" : "degrees", "default_value" : -45, "title" : "disturbance amplitude" })     * dy.float64(math.pi / 180.0)
 sample_disturbance     = dy.convert(dy.system_input( baseDatatype ).set_name('sample_disturbance').set_properties({ "range" : [0, 300], "unit" : "samples", "default_value" : 0, "title" : "time of disturbance" }), target_type=dy.DataTypeInt32(1) )
 
+# parameters
 wheelbase = 3.0
 
-#
-# define the path
-#
-
-Td = 0.1
-path_x = example_data.output['X'] # np.concatenate(( ra(360, 0, 80, Ts=Td),  )) 
-path_y = example_data.output['Y'] # np.concatenate(( co(50, 0, Ts=Td), cosra(100, 0, 1, Ts=Td), co(50, 1, Ts=Td), cosra(100, 1, 0, Ts=Td), co(60,0, Ts=Td) ))
-path_distance = example_data.output['D'] #  np.concatenate((np.zeros(1), np.cumsum( np.sqrt( np.square( np.diff(path_x) ) + np.square( np.diff(path_y) ) ) ) ))
-N_path = example_data.Nmax # np.size(path_x)
-
-path_x_storage = dy.memory(datatype=dy.DataTypeFloat64(1), constant_array=path_x )
-path_y_storage = dy.memory(datatype=dy.DataTypeFloat64(1), constant_array=path_y )
-path_distance_storage = dy.memory(datatype=dy.DataTypeFloat64(1), constant_array=path_distance )
-
+# create storage for the reference path:
+# distance on path (D), position (X/Y), path orientation (PSI), curvature (K)
 path = {}
-path['D'] = path_distance_storage
-path['X'] = path_x_storage
-path['Y'] = path_y_storage
+path['D'] = dy.memory(datatype=dy.DataTypeFloat64(1), constant_array=example_data.output['D'] )
+path['X'] = dy.memory(datatype=dy.DataTypeFloat64(1), constant_array=example_data.output['X'] )
+path['Y'] = dy.memory(datatype=dy.DataTypeFloat64(1), constant_array=example_data.output['Y'] )
+path['PSI'] = dy.memory(datatype=dy.DataTypeFloat64(1), constant_array=example_data.output['PSI'] )
+path['K'] = dy.memory(datatype=dy.DataTypeFloat64(1), constant_array=example_data.output['K'] )
 
-path['samples'] = N_path
+path['samples'] = example_data.Nmax
 
-
-# create placeholder for the plant output signal
+# create placeholders for the plant output signals
 x   = dy.signal()
 y   = dy.signal()
 psi = dy.signal()
 
-#
-# track the evolution of x/y along the given path
-#
-
-def distance_between( x1, y1, x2, y2 ):
-
-    dx_ = x1 - x2
-    dy_ = y1 - y2
-
-    return dy.sqrt(  dx_*dx_ + dy_*dy_ )
-
-
-def tracker(path, x, y):
-    index_track   = dy.signal()
-
-    with dy.sub_loop( max_iterations=1000 ) as system:
-
-        index_increment = dy.int32(1) # positive increment assuming positive velocity
-
-        Delta_index = dy.sum(index_increment, initial_state=-1 )
-        Delta_index_previous_step = Delta_index - index_increment
-
-        x_test = dy.memory_read( memory=path['X'], index=index_track + Delta_index ) 
-        y_test = dy.memory_read( memory=path['Y'], index=index_track + Delta_index )
-
-        distance = distance_between( x_test, y_test, x, y )
-
-        distance_previous_step = dy.delay(distance, initial_state=100000)
-        minimal_distance_reached = distance_previous_step < distance
-
-        # introduce names
-        distance.set_name('tracker_distance')
-        minimal_distance_reached.set_name('minimal_distance_reached')
-
-        # break condition
-        system.loop_until( minimal_distance_reached )
-
-        # return        
-        system.set_outputs([ Delta_index_previous_step, distance_previous_step ])
-
-
-    Delta_index = system.outputs[0].set_name('tracker_Delta_index')
-    distance = system.outputs[1].set_name('distance')
-
-    index_track_next = index_track + Delta_index 
-
-    index_track << dy.delay(index_track_next, initial_state=1)
-
-    return index_track, Delta_index, distance
-
-
+# track the evolution of the closest point on the path to the vehicles position
 tracked_index, Delta_index, closest_distance = tracker(path, x, y)
 
-
-# 
-x_r, y_r, psi_r = sample_path(path_distance_storage, path_x_storage, path_y_storage, index=tracked_index )
+# get the reference
+x_r, y_r, psi_r = sample_path(path, index=tracked_index )
 
 # add sign information to the distance
 Delta_l = distance_to_Delta_l( closest_distance, psi_r, x_r, y_r, x, y )
 
-
-
-#
-# controller
-#
-
-# feedback
+# feedback control
 Delta_u = dy.PID_controller(r=dy.float64(0.0), y=Delta_l, Ts=0.01, kp=k_p, ki = dy.float64(0.0), kd = dy.float64(0.0)) # 
 
 # path tracking
 steering = psi_r - psi + Delta_u
 steering = dy.unwrap_angle(angle=steering, normalize_around_zero = True) 
 
-
 #
-# The model of the vehicle
+# The model of the vehicle including a disturbance
 #
 
-
-#
+# model the disturbance
 disturbance_transient = np.concatenate(( cosra(50, 0, 1.0), co(10, 1.0), cosra(50, 1.0, 0) ))
 steering_disturbance, i = dy.play(disturbance_transient, start_trigger=dy.counter() == sample_disturbance, auto_start=False)
-#steering_disturbance, i = dy.play(disturbance_transient, auto_start=False)
 
 # apply disturbance to the steering input
 disturbed_steering = steering + steering_disturbance * disturbance_amplitude
 
-#
+# steering angle limit
 disturbed_steering = dy.saturate(u=disturbed_steering, lower_limit=-math.pi/2.0, uppper_limit=math.pi/2.0)
 
+# the model of the vehicle
+x_, y_, psi_ = discrete_time_bicycle_model(disturbed_steering, velocity, wheelbase)
 
-def discrete_time_bicycle_model(delta, v):
-    x   = dy.signal()
-    y   = dy.signal()
-    psi = dy.signal()
-
-    # bicycle model
-    x_dot   = v * dy.cos( delta + psi )
-    y_dot   = v * dy.sin( delta + psi )
-    psi_dot = v / dy.float64(wheelbase) * dy.sin( delta )
-
-    # integrators
-    sampling_rate = 0.01
-    x    << dy.euler_integrator(x_dot,   sampling_rate, 0.0)
-    y    << dy.euler_integrator(y_dot,   sampling_rate, 0.0)
-    psi  << dy.euler_integrator(psi_dot, sampling_rate, 0.0)
-
-    return x, y, psi
-
-
-x_, y_, psi_ = discrete_time_bicycle_model(disturbed_steering, velocity)
-
+# close the feedback loops
 x << x_
 y << y_
 psi << psi_
@@ -182,10 +84,8 @@ psi << psi_
 dy.set_primary_outputs([ x, y, x_r, y_r, psi, psi_r, steering, Delta_l, steering_disturbance, disturbed_steering, tracked_index, Delta_index], 
         ['x', 'y', 'x_r', 'y_r', 'psi', 'psi_r', 'steering', 'Delta_l', 'steering_disturbance', 'disturbed_steering', 'tracked_index', 'Delta_index'])
 
-#
+# generate code
 sourcecode, manifest = dy.generate_code(template=dy.WasmRuntime(enable_tracing=False), folder="generated/", build=True)
 
-# print the sourcecode (main.cpp)
-#print(Style.DIM + sourcecode)
-
+#
 dy.clear()
--- vehicle_lib/vehicle_lib.py
+++ vehicle_lib/vehicle_lib.py
@@ -19,6 +19,73 @@ def ra(time, val1, val2, Ts=1.0):
 
 
 
+def discrete_time_bicycle_model(delta, v, wheelbase):
+    x   = dy.signal()
+    y   = dy.signal()
+    psi = dy.signal()
+
+    # bicycle model
+    x_dot   = v * dy.cos( delta + psi )
+    y_dot   = v * dy.sin( delta + psi )
+    psi_dot = v / dy.float64(wheelbase) * dy.sin( delta )
+
+    # integrators
+    sampling_rate = 0.01
+    x    << dy.euler_integrator(x_dot,   sampling_rate, 0.0)
+    y    << dy.euler_integrator(y_dot,   sampling_rate, 0.0)
+    psi  << dy.euler_integrator(psi_dot, sampling_rate, 0.0)
+
+    return x, y, psi
+
+
+
+def distance_between( x1, y1, x2, y2 ):
+
+    dx_ = x1 - x2
+    dy_ = y1 - y2
+
+    return dy.sqrt(  dx_*dx_ + dy_*dy_ )
+
+
+def tracker(path, x, y):
+    index_track   = dy.signal()
+
+    with dy.sub_loop( max_iterations=1000 ) as system:
+
+        index_increment = dy.int32(1) # positive increment assuming positive velocity
+
+        Delta_index = dy.sum(index_increment, initial_state=-1 )
+        Delta_index_previous_step = Delta_index - index_increment
+
+        x_test = dy.memory_read( memory=path['X'], index=index_track + Delta_index ) 
+        y_test = dy.memory_read( memory=path['Y'], index=index_track + Delta_index )
+
+        distance = distance_between( x_test, y_test, x, y )
+
+        distance_previous_step = dy.delay(distance, initial_state=100000)
+        minimal_distance_reached = distance_previous_step < distance
+
+        # introduce signal names
+        distance.set_name('tracker_distance')
+        minimal_distance_reached.set_name('minimal_distance_reached')
+
+        # break condition
+        system.loop_until( minimal_distance_reached )
+
+        # return        
+        system.set_outputs([ Delta_index_previous_step, distance_previous_step ])
+
+
+    Delta_index = system.outputs[0].set_name('tracker_Delta_index')
+    distance = system.outputs[1].set_name('distance')
+
+    index_track_next = index_track + Delta_index 
+
+    index_track << dy.delay(index_track_next, initial_state=1)
+
+    return index_track, Delta_index, distance
+
+
 
 
 def lookup_distance_index( path_distance_storage, path_x_storage, path_y_storage, distance ):
@@ -116,23 +183,30 @@ def lookup_closest_point( N, path_distance_storage, path_x_storage, path_y_stora
 
 
 
-def sample_path(path_distance_storage, path_x_storage, path_y_storage, index):
+# def sample_path(path_distance_storage, path_x_storage, path_y_storage, index):
 
-    y1 = dy.memory_read( memory=path_y_storage, index=index ) 
-    y2 = dy.memory_read( memory=path_y_storage, index=index + dy.int32(1) )
+#     y1 = dy.memory_read( memory=path_y_storage, index=index ) 
+#     y2 = dy.memory_read( memory=path_y_storage, index=index + dy.int32(1) )
 
-    x1 = dy.memory_read( memory=path_x_storage, index=index ) 
-    x2 = dy.memory_read( memory=path_x_storage, index=index + dy.int32(1) )
+#     x1 = dy.memory_read( memory=path_x_storage, index=index ) 
+#     x2 = dy.memory_read( memory=path_x_storage, index=index + dy.int32(1) )
 
-    Delta_x = x2 - x1
-    Delta_y = y2 - y1
+#     Delta_x = x2 - x1
+#     Delta_y = y2 - y1
 
-    psi_r = dy.atan2(Delta_y, Delta_x)
-    x_r = x1
-    y_r = y1
+#     psi_r = dy.atan2(Delta_y, Delta_x)
+#     x_r = x1
+#     y_r = y1
+
+#     return x_r, y_r, psi_r
+
+def sample_path(path, index):
+
+    y_r = dy.memory_read( memory=path['Y'], index=index ) 
+    x_r = dy.memory_read( memory=path['X'], index=index ) 
+    psi_r = dy.memory_read( memory=path['PSI'], index=index ) 
 
     return x_r, y_r, psi_r
-
 
 
 def distance_to_Delta_l( distance, psi_r, x_r, y_r, x, y ):
