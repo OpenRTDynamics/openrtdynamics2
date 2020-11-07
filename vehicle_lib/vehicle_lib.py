96374cb922a2df824008d153b5f5f31a6c0522cd
Christian Klauer <chr.klauer@gmail.com>
2020-11-07 10:34:20 +0100

    vehicle example:tracker_distance_ahead - I

--- vehicle_lib/vehicle_lib.py
+++ vehicle_lib/vehicle_lib.py
@@ -52,10 +52,10 @@ def tracker(path, x, y):
 
     with dy.sub_loop( max_iterations=1000 ) as system:
 
-        index_increment = dy.int32(1) # positive increment assuming positive velocity
+        search_index_increment = dy.int32(1) # positive increment assuming positive velocity
 
-        Delta_index = dy.sum(index_increment, initial_state=-1 )
-        Delta_index_previous_step = Delta_index - index_increment
+        Delta_index = dy.sum(search_index_increment, initial_state=-1 )
+        Delta_index_previous_step = Delta_index - search_index_increment
 
         x_test = dy.memory_read( memory=path['X'], index=index_track + Delta_index ) 
         y_test = dy.memory_read( memory=path['Y'], index=index_track + Delta_index )
@@ -86,9 +86,83 @@ def tracker(path, x, y):
     return index_track, Delta_index, distance
 
 
+def tracker_distance_ahead(path, current_index, distance_ahead):
+    """
+
+                  <----- Delta_index_track ----->
+        array: X  X  X  X  X  X  X  X  X  X  X  X  X  X  X 
+                  ^               
+            current_index
+    """
+
+    if 'Delta_d' in path:
+        # constant sampling interval in distance
+        # computation can be simplified
+        pass
 
 
-def lookup_distance_index( path_distance_storage, path_x_storage, path_y_storage, distance ):
+    def J( index ):
+
+        d_test = dy.memory_read( memory=path['D'], index=index ) 
+        distance = dy.abs( d_test - dy.float64(distance_ahead) )
+
+        return distance
+    
+
+    Delta_index_track   = dy.signal()
+
+    # initialize J_star
+    J_star_0 = J(current_index + Delta_index_track)
+
+    #
+    # compute the direction in which J has its decent
+    # if true: with increasing index J increases  --> decrease search index
+    # if false: with increasing index J decreases --> increase search index
+    #
+    direction_flag = J(current_index + Delta_index_track + dy.int32(1)) > J_star_0
+
+    search_index_increment = dy.int32(1) 
+    search_index_increment = dy.conditional_overwrite(search_index_increment, direction_flag, dy.int32(-1) )
+
+    # loop to find the minimum of J
+    with dy.sub_loop( max_iterations=1000 ) as system:
+
+
+        # J_star(k) - the smallest J found so far
+        J_star = dy.signal()
+        # J_star.inherit_datatype(J_star_0)
+
+        # inc- / decrease the search index
+        Delta_index = dy.sum(search_index_increment, initial_state=0 )
+
+        # sample the cost function and check if it got smaller in this step
+        J_to_verify = J( current_index + Delta_index_track + Delta_index )
+        step_caused_improvment = J_to_verify < J_star
+
+        # replace the 
+        J_star_next = dy.conditional_overwrite( J_star, step_caused_improvment, J_to_verify )
+
+        # state for J_star
+        J_star << dy.delay( J_star_next, initial_state=J_star_0 )
+
+
+        # loop break condition
+        system.loop_until( dy.logic_not( step_caused_improvment ) )
+
+        # return the results computed in the loop        
+        system.set_outputs([ Delta_index ])
+
+
+
+    Delta_index_track_next = Delta_index_track + system.outputs[0]
+    Delta_index_track << dy.delay(Delta_index_track_next, initial_state=0)
+
+
+    return Delta_index_track
+
+
+
+def global_lookup_distance_index( path_distance_storage, path_x_storage, path_y_storage, distance ):
     #
     source_code = """
         index = 0;
@@ -114,91 +188,7 @@ def lookup_distance_index( path_distance_storage, path_x_storage, path_y_storage
     return index
 
 
-def lookup_closest_point( N, path_distance_storage, path_x_storage, path_y_storage, x, y ):
-    #
-    source_code = """
-        // int N = 360;
-        int N = *(&path_distance_storage + 1) - path_distance_storage;
 
-        int i = 0;
-        double min_distance_to_path = 1000000;
-        int min_index = 0;
-
-        for (i = 0; i < N; ++i ) {
-            double dx = path_x_storage[i] - x_;
-            double dy = path_y_storage[i] - y_;
-            double distance_to_path = sqrt( dx * dx + dy * dy );
-
-            if ( distance_to_path < min_distance_to_path ) {
-                min_distance_to_path = distance_to_path;
-                min_index = i;
-            }
-        }
-
-        double dx_p1, dy_p1, dx_p2, dy_p2, distance_to_path_p1, distance_to_path_p2;
-
-        dx_p1 = path_x_storage[min_index + 1] - x_;
-        dy_p1 = path_y_storage[min_index + 1] - y_;
-        distance_to_path_p1 = sqrt( dx_p1 * dx_p1 + dy_p1 * dy_p1 );
-
-        dx_p2 = path_x_storage[min_index - 1] - x_;
-        dy_p2 = path_y_storage[min_index - 1] - y_;
-        distance_to_path_p2 = sqrt( dx_p2 * dx_p2 + dy_p2 * dy_p2 );
-
-        int interval_start, interval_stop;
-        if (distance_to_path_p1 < distance_to_path_p2) {
-            // minimal distance in interval [min_index, min_index + 1]
-            interval_start = min_index;
-            interval_stop  = min_index + 1;
-        } else {
-            // minimal distance in interval [min_index - 1, min_index]
-            interval_start = min_index - 1;
-            interval_stop  = min_index;
-        }
-
-        // linear interpolation
-        double dx = path_x_storage[interval_stop] - path_x_storage[interval_start] ;
-        double dy = path_y_storage[interval_stop] - path_y_storage[interval_start] ;
-
-
-
-        index_start   = interval_start;
-        index_closest = min_index;
-        distance      = min_distance_to_path;
-
-    """
-    array_type = dy.DataTypeArray( N, datatype=dy.DataTypeFloat64(1) )
-    outputs = dy.generic_cpp_static(input_signals=[ path_distance_storage, path_x_storage, path_y_storage, x, y ], 
-                                    input_names=[ 'path_distance_storage', 'path_x_storage', 'path_y_storage', 'x_', 'y_' ], 
-                                    input_types=[ array_type, array_type, array_type, dy.DataTypeFloat64(1), dy.DataTypeFloat64(1) ], 
-                                    output_names=[ 'index_closest', 'index_start', 'distance'],
-                                    output_types=[ dy.DataTypeInt32(1), dy.DataTypeInt32(1), dy.DataTypeFloat64(1) ],
-                                    cpp_source_code = source_code )
-
-    index_start = outputs[0]
-    index_closest = outputs[1]
-    distance     = outputs[2]
-
-    return index_closest, distance, index_start
-
-
-
-# def sample_path(path_distance_storage, path_x_storage, path_y_storage, index):
-
-#     y1 = dy.memory_read( memory=path_y_storage, index=index ) 
-#     y2 = dy.memory_read( memory=path_y_storage, index=index + dy.int32(1) )
-
-#     x1 = dy.memory_read( memory=path_x_storage, index=index ) 
-#     x2 = dy.memory_read( memory=path_x_storage, index=index + dy.int32(1) )
-
-#     Delta_x = x2 - x1
-#     Delta_y = y2 - y1
-
-#     psi_r = dy.atan2(Delta_y, Delta_x)
-#     x_r = x1
-#     y_r = y1
-
-#     return x_r, y_r, psi_r
 
 def sample_path(path, index):
 
