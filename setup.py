2cad16a0fa0fe64fc5a7f099dca00c877afedd61
Christian Klauer <chr.klauer@gmail.com>
2020-11-29 21:48:42 +0100

    0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="openrtdynamics2",
-    version="0.0.7",
+    version="0.0.8",
     author="Christian Klauer",
     author_email="chr@gmail.com",
     description="OpenRTDynamics 2",
@@ -17,6 +17,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy', 'control', 'colorama'
     ],
     python_requires='>=3.6',
 )
