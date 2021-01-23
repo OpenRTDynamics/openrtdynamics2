dd63cd842993a7bba493f09793db50dbcbfc801e
Christian Klauer <chr.klauer@gmail.com>
2021-01-23 13:11:28 +0100

    first test

---
+++ tests/test_basic.py
@@ -0,0 +1,30 @@
+from .context import openrtdynamics2
+import openrtdynamics2.lang as dy
+
+
+def test_basic_code_gen():
+    # create a new system
+    system = dy.enter_system()
+
+    # define system inputs
+    input1               = dy.system_input( dy.DataTypeFloat64(1), name='input1', default_value=5.0,  value_range=[0, 25], title="input #1")
+
+    # the diagram
+    tmp = input1 * dy.float64(2.0)
+    tmp.set_name("some_name")
+    output = dy.delay(tmp)
+
+    # define output(s)
+    dy.append_primay_ouput(output, 'outputs')
+
+    # generate code
+    code_gen_results = dy.generate_code(template=dy.WasmRuntime(), folder="generated/", build=False)
+    sourcecode, manifest = code_gen_results['sourcecode'], code_gen_results['manifest']
+
+    # clear workspace
+    dy.clear()
+
+    # raise BaseException('testfail')
+
+
+
