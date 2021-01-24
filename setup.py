b41067dcf72f64cca7c0b3e94f7b20685f7b2395
Christian Klauer <chr.klauer@gmail.com>
2021-01-24 11:20:56 +0100

    0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="openrtdynamics2",
-    version="0.1.0",
+    version="0.2.0",
     author="Christian Klauer",
     author_email="chr@gmail.com",
     description="OpenRTDynamics 2",
